055f1ac39c6763ab6e98ae990dbb5ee4a22f86a5
Guilherme <guilhermelopes035@gmail.com>
2024-11-18 22:58:29 -0300

    fix: ajusta cors

--- musicas.php
+++ musicas.php
@@ -2,9 +2,15 @@
 require_once(__DIR__ . '/configs/utils.php');
 require_once(__DIR__ . '/model/Musica.php');
 
-header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json; charset=UTF-8");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
 
 try {
     $data = handleJSONInput();
